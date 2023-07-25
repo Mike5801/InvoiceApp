0ba51b53b5ef57cec3d9d425016c64ff3b266c25
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-24 19:49:06 -0600

    add tailwind configuration

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
-import cors from "cors";
 import dotenv from "dotenv";
-import helmet from "helmet";
+import path from "path";
+import { fileURLToPath } from "url";
 
 /* global settings */
 dotenv.config();
-const corsOptions = {
-  origin: [process.env.ORIGIN],
-  credentials: true,
-};
-
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.set("view engine", "pug");
 
 /* Middleware settings */
 app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use("/", express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5001;
 
 /* Routes */
-import invoiceRoutes from "./routes/invoice.routes.js"
+import invoiceRoutes from "./routes/invoice.routes.js";
 
-app.use("/invoice", invoiceRoutes)
+app.use("/invoice", invoiceRoutes);
+
+app.get("/", (req, res) => {
+  res.render('pages/Main/index');
+});
 
 app.listen(PORT, () => console.log(`Running Success, Server Port: ${PORT}`));
-
-/* MONGOOSE SETUP */
-//import mongoose from "mongoose";
-// mongoose.set('strictQuery', false);
-// mongoose.connect(process.env.MONGO_URL, {
-  //   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   dbName: process.env.DB_NAME
-// }).then(() => {
-//   app.listen(PORT, () => console.log(`Server Port: ${ PORT }`));
-// }).catch((error) => console.log(`${ error } did not connect`));
