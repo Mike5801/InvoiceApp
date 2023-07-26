8cc1a1e4881179a85986ef7f1055dc3e1c6a1ec2
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-26 13:53:41 -0600

    refactor walmart controller

--- controllers/invoice.controllers.js
+++ controllers/invoice.controllers.js
@@ -11,10 +11,15 @@ import {
   handleSendingInvoiceError,
 } from "../utils/errorHandling.utils.js";
 
+import walmartNavigation from "../utils/walmartNavigation.utils.js";
+
 const viewVariables = {
   status: null,
   message: "",
 };
+
+const RFC = process.env.RFC;
+const CP = process.env.CP;
 
 export const getCostcoInvoicePage = (req, res) => {
   viewVariables.status = null;
@@ -99,76 +104,16 @@ export const getWalmartInvoice = async (req, res) => {
   const { transaction, ticket } = req.body;
 
   try {
-    /* Go to Walmart invoice page */
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("https://facturacion.walmartmexico.com.mx/Default.aspx");
 
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Enter");
+    await walmartNavigation.goToPage(page, "https://facturacion.walmartmexico.com.mx/Default.aspx", "https://facturacion.walmartmexico.com.mx/frmDatos.aspx");
 
-    await page.goto("https://facturacion.walmartmexico.com.mx/frmDatos.aspx");
+    await walmartNavigation.enterTicketInformation(browser, page, RFC, CP, ticket, transaction);
 
-    await page.waitForTimeout(3000);
+    await walmartNavigation.enterInvoiceInformation(browser, page);
 
-    /* Enter initial invoice data */
-    const inputRfc = await page.$("#ctl00_ContentPlaceHolder1_txtMemRFC");
-    if (!inputRfc) handleInitialInformationError();
-    
-    const inputCP = await page.$("#ctl00_ContentPlaceHolder1_txtCP");
-    const inputTicket = await page.$("#ctl00_ContentPlaceHolder1_txtTC");
-    const inputTransaction = await page.$("#ctl00_ContentPlaceHolder1_txtTR");
-    const buttonContinue = await page.$(
-      "#ctl00_ContentPlaceHolder1_btnAceptar"
-    );
-
-    await inputRfc.type(process.env.RFC);
-    await inputCP.type(process.env.CP);
-    await inputTicket.type(ticket);
-    await inputTransaction.type(transaction);
-    await buttonContinue.click();
-
-    await page.waitForTimeout(5000);
-
-    /* Enter specific invoice data (Régimen Fiscal and use of invoice) */
-    const regimenFiscalDropdown = await page.$(
-      "#ctl00_ContentPlaceHolder1_ddlregimenFiscal"
-    );
-    if (!regimenFiscalDropdown) handleClientInformationError();
-
-    await page.select("#ctl00_ContentPlaceHolder1_ddlregimenFiscal", "621");
-    await page.waitForTimeout(1000);
-    await page.select("#ctl00_ContentPlaceHolder1_ddlusoCFDI", "G03");
-    await page.waitForTimeout(1000);
-
-    const buttonAccept = await page.$("#ctl00_ContentPlaceHolder1_btnAceptar");
-    await buttonAccept.click();
-
-    await page.waitForTimeout(1000);
-
-    const buttonConfirm = await page.$("#ctl00_btnContinuar");
-    await buttonConfirm.click();
-
-    await page.waitForTimeout(5000);
-
-    /* Send invoice  */
-    const buttonEmail = await page.$("#ctl00_ContentPlaceHolder1_rdCorreo");
-    if (!buttonEmail) handleSendingInvoiceError();
-
-    await buttonEmail.click();
-
-    const buttonInvoice = await page.$(
-      "#ctl00_ContentPlaceHolder1_btnFacturar"
-    );
-    await buttonInvoice.click();
-
-    const buttonClose = await page.$("#ctl00_ContentPlaceHolder1_btnCerrar");
-    await buttonClose.click();
-
-    await page.waitForTimeout(5000);
+    await walmartNavigation.sendInvoice(browser, page);
 
     await browser.close();
 
